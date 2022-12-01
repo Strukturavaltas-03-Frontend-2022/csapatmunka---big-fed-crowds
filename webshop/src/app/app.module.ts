e6cc7b8970fb18d5b6a88547ce11a61dd7b45795
Anankaaa <hegyaljai83@hotmail.com>
2022-12-01 19:31:07 +0100

    Components generated

--- webshop/src/app/app.module.ts
+++ webshop/src/app/app.module.ts
@@ -6,13 +6,33 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './common/sidebar/sidebar.component';
 import { HomeComponent } from './page/home/home.component';
 import { HeaderComponent } from './common/header/header.component';
+import { ProductListComponent } from './page/lists/product-list/product-list.component';
+import { CustomerListComponent } from './page/lists/customer-list/customer-list.component';
+import { OrderListComponent } from './page/lists/order-list/order-list.component';
+import { BillListComponent } from './page/lists/bill-list/bill-list.component';
+import { EditProductComponent } from './page/editors/edit-product/edit-product.component';
+import { EditCustomerComponent } from './page/editors/edit-customer/edit-customer.component';
+import { EditOrderComponent } from './page/editors/edit-order/edit-order.component';
+import { EditBillComponent } from './page/editors/edit-bill/edit-bill.component';
+import { FilterPipe } from './pipe/filter.pipe';
+import { SorterPipe } from './pipe/sorter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProductListComponent,
+    CustomerListComponent,
+    OrderListComponent,
+    BillListComponent,
+    EditProductComponent,
+    EditCustomerComponent,
+    EditOrderComponent,
+    EditBillComponent,
+    FilterPipe,
+    SorterPipe
   ],
   imports: [
     BrowserModule,
