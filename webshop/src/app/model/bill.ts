048f307774ff79e3e809453482d4197705771d55
utnesz <90829509+utnesz@users.noreply.github.com>
2022-12-02 12:44:14 +0100

    Classes are filled up

---
+++ webshop/src/app/model/bill.ts
@@ -0,0 +1,9 @@
+import { Order } from './../model/order';
+export class Bill {
+  [x: string]: any;
+  id: number = 0;
+  orderID: Order = new Order();
+  amount: number = 0;
+  status: string[] = [];
+}
+
