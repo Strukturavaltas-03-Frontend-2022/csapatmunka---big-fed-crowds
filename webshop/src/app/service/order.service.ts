01fe710c50c8ada2245b6db3990e113820ef9894
Anankaaa <hegyaljai83@hotmail.com>
2022-12-03 11:18:41 +0100

    order-list basics, app-routing update

--- webshop/src/app/service/order.service.ts
+++ webshop/src/app/service/order.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Order } from '../model/order';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
-  constructor() { }
+  apiUrl: string = environment.apiUrl
+  entity: string = 'order'
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Order[]> {
+    return this.http.get<Order[]>(`${this.apiUrl}${this.entity}`)
+  }
+
+ create(order: Order): Observable<Order>{
+    return this.http.post<Order>(
+    `${this.apiUrl}${this.entity}`,
+    order)
+    }
+
+ update(order: Order): Observable<Order>{
+    return this.http.patch<Order>(
+    `${this.apiUrl}${this.entity}/${order.id}`,
+    order)
+    }
+
+ delete(order: Order): Observable<Order>{
+    return this.http.delete<Order>(`${this.apiUrl}${this.entity}/${order.id}`)
+    }
 }
