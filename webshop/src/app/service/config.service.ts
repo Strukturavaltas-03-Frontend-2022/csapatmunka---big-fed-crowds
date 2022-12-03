a4dbf4c74b7c0b1905b05f4636252af83b0b21c8
Anankaaa <hegyaljai83@hotmail.com>
2022-12-03 08:28:40 +0100

    Config service update

--- webshop/src/app/service/config.service.ts
+++ webshop/src/app/service/config.service.ts
@@ -19,6 +19,32 @@ export class ConfigService {
     { title: 'Active', key: 'active' },
   ];
 
+  productTableColumns: ITableColumn[] = [
+    { title: 'ID', key: 'id' },
+    { title: 'Name', key: 'name' },
+    { title: 'Type', key: 'type' },
+    { title: 'Category ID', key: 'catId' },
+    { title: 'Description', key: 'description' },
+    { title: 'Price', key: 'price' },
+    { title: 'Featured', key: 'featured' },
+    { title: 'Active', key: 'active' },
+  ];
+
+  billTableColumns: ITableColumn[] = [
+    { title: 'ID', key: 'id' },
+    { title: 'Order ID', key: 'orderID' },
+    { title: 'Amount', key: 'amount' },
+    { title: 'Status', key: 'status' },
+  ];
+
+  orderTableColumns: ITableColumn[] = [
+    { title: 'ID', key: 'id' },
+    { title: 'Customer ID', key: 'customerID' },
+    { title: 'Product ID', key: 'prouctID' },
+    { title: 'Amount', key: 'amount' },
+    { title: 'Shipped', key: 'shipped' },
+  ];
+
 
   constructor() { }
 }
