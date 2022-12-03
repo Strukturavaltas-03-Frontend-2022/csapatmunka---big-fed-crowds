7ff65ed3b8d0f82a684fce36b80d92f3d9d94e69
utnesz <90829509+utnesz@users.noreply.github.com>
2022-12-03 11:58:52 +0100

    still working on products

--- webshop/src/app/page/lists/product-list/product-list.component.ts
+++ webshop/src/app/page/lists/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { productHeaders } from 'src/app/model/product';
+
 import { Component, OnInit, Input, inject } from '@angular/core';
 import { combineLatest, map } from 'rxjs';
 import { Product } from 'src/app/model/product';
@@ -6,6 +6,7 @@ import { Category } from 'src/app/model/category';
 import { ProductService } from 'src/app/service/product.service';
 import { CategoryService } from 'src/app/service/category.service';
 import { Observable } from 'rxjs';
+import { ConfigService } from 'src/app/service/config.service';
 
 @Component({
   selector: 'app-product-list',
@@ -13,29 +14,27 @@ import { Observable } from 'rxjs';
   styleUrls: ['./product-list.component.scss'],
 })
 export class ProductListComponent implements OnInit {
+  productList$: Observable<Product[]> = this.productService.getAll();
 
-  @Input() productHeaders: string[] = [];
-
-  product$: Observable<Product[]> = this.productService.getAll();
+  columns = this.configService.productTableColumns;
 
   constructor(
     private productService: ProductService,
+    private configService: ConfigService,
     private categoryService: CategoryService
   ) {}
 
-  headers = this.productHeaders;
-
-  // product$ = combineLatest({
-  //   cat: this.categoryService.getAll(),
-  //   prod: this.productService.getAll(),
-  // }).pipe(
-  //   map((result) =>
-  //     result.prod.map((product) => {
-  //       product.catId = result.cat.find((c) => (c.id = product.id));
-  //       return product;
-  //     })
-  //   )
-  // );
+//productList$ = combineLatest({
+//  cat: this.categoryService.getAll(),
+//  prod: this.productService.getAll(),
+//}).pipe(
+//  map(result =>
+//    result.prod.map(product => {
+//      product.catId = result.cat.find(c => c.id === product.id);
+//      return product;
+//    })
+//  )
+//);
 
   ngOnInit(): void {}
 }
