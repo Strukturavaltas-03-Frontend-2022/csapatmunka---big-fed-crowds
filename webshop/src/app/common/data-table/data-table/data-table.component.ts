de892b43994960448d919db7795ba5ec0775159c
utnesz <90829509+utnesz@users.noreply.github.com>
2022-12-04 10:50:03 +0100

    working on filters on data-table

--- webshop/src/app/common/data-table/data-table/data-table.component.ts
+++ webshop/src/app/common/data-table/data-table/data-table.component.ts
@@ -9,41 +9,41 @@ export interface ITableCol {
 @Component({
   selector: 'app-data-table',
   templateUrl: './data-table.component.html',
-  styleUrls: ['./data-table.component.scss']
+  styleUrls: ['./data-table.component.scss'],
 })
-export class DataTableComponent<T extends { [x: string]: any }> implements OnInit {
-
+export class DataTableComponent<T extends { [x: string]: any }>
+  implements OnInit
+{
   @Input() listers: T[] = [];
   @Input() columns: ITableCol[] = [];
 
   sortKey: string = '';
   sortDirection: number = 1;
 
+  filterPhrase: string = '';
+
   page: number = 1;
 
   @Output() onEdit: EventEmitter<T> = new EventEmitter();
   @Output() onDelete: EventEmitter<T> = new EventEmitter();
 
-
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   raiseEdit(row: T): void {
     this.onEdit.emit(row);
   }
 
-  raiseDelete(row:T): void {
+  raiseDelete(row: T): void {
     this.onDelete.emit(row);
   }
 
   startSort(key: string): void {
     if (key === this.sortKey) {
-      this.sortDirection *= -1
+      this.sortDirection *= -1;
     } else {
-      this.sortDirection = 1
+      this.sortDirection = 1;
     }
 
-    this.sortKey = key
+    this.sortKey = key;
   }
 }
