305bb99fc471447e9cd7849f1faaa7e83639dfc4
utnesz <90829509+utnesz@users.noreply.github.com>
2022-12-05 20:24:42 +0100

    working on cutomerlist

--- webshop/src/app/page/lists/customer-list/customer-list.component.ts
+++ webshop/src/app/page/lists/customer-list/customer-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Customer } from 'src/app/model/customer';
 import { ConfigService } from 'src/app/service/config.service';
@@ -11,10 +11,24 @@ import { Router } from '@angular/router';
   templateUrl: './customer-list.component.html',
   styleUrls: ['./customer-list.component.scss'],
 })
-export class CustomerListComponent {
+export class CustomerListComponent<T extends { [x: string]: any }>
+  implements OnInit
+{
+  @Input() listers: T[] = [];
+  @Input() headers: string[] = [];
   customerList$: Observable<Customer[]> = this.customerService.getAll();
 
   columns = this.configService.customerTableColumns;
+
+  sortKey: string = '';
+  sortDirection: number = 1;
+
+  key: string = '';
+  phrases: string[] = [];
+  filterPhrase: string = '';
+  sortHeader: string = '';
+
+  page: number = 1;
 
   constructor(
     private customerService: CustomerService,
@@ -23,15 +37,40 @@ export class CustomerListComponent {
     private router: Router
   ) {}
 
-  onEdit(customer: Customer):void {
-    this.router.navigate(['/edit-customer', customer.id])
-
+  onEdit(customer: Customer): void {
+    this.router.navigate(['/edit-customer', customer.id]);
   }
 
-  onDelete(customer: Customer):void {
-    this.customerService.delete( customer).subscribe(
-      () => this.customerList$ = this.customerService.getAll(),
-    )
-    this.toastr.warning( 'Customer is deleted!', 'WARNING!',);
+  onDelete(customer: Customer): void {
+    this.customerService
+      .delete(customer)
+      .subscribe(() => (this.customerList$ = this.customerService.getAll()));
+    this.toastr.warning('Customer is deleted!', 'WARNING!');
+  }
+
+  ngOnInit(): void {}
+
+
+  startSort(key: string): void {
+    if (key === this.sortKey) {
+      this.sortDirection *= -1;
+    } else {
+      this.sortDirection = 1;
+    }
+
+    this.sortKey = key;
+  }
+
+  onPhraseChange(header: string, index: number) {
+    this.key = header;
+    this.filterPhrase = this.phrases[index];
+  }
+
+  clearFilterAndSortInputs() {
+    this.filterPhrase = '';
+    this.key = '';
+    for (let i = 0; i < this.phrases.length; i++) {
+      this.phrases[i] = '';
+    }
   }
 }
