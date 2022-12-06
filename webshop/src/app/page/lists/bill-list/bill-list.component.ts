ed4329e6aa1d9ab617f7be912ee77e8af48c8eb1
Istvan888 <marai.istvanp@gmail.com>
2022-12-06 20:06:18 +0100

    edit bill is done

--- webshop/src/app/page/lists/bill-list/bill-list.component.ts
+++ webshop/src/app/page/lists/bill-list/bill-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { Bill } from 'src/app/model/bill';
 import { BillService } from 'src/app/service/bill.service';
@@ -18,7 +20,19 @@ export class BillListComponent {
 
   constructor(
     private billService: BillService,
-    private configService: ConfigService
+    private configService: ConfigService,
+    private toastr: ToastrService,
+    private router: Router,
   ) {}
 
+  onEdit(bill: Bill):void {
+    this.router.navigate(['/edit-bill', bill.id])
+   }
+   
+   onDelete(bill: Bill):void {
+     this.billService.delete(bill).subscribe(
+       () => this.billList$ = this.billService.getAll(),
+     )
+     this.toastr.warning( 'Bill is deleted!', 'WARNING!',);
+   }
 }
