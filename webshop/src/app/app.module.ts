afdcab62482dfb95da1c874ba849cf6f3796ba7e
Anankaaa <hegyaljai83@hotmail.com>
2022-12-08 20:22:59 +0100

    table sum

--- webshop/src/app/app.module.ts
+++ webshop/src/app/app.module.ts
@@ -25,6 +25,7 @@ import { FilterPipe } from './pipe/filter.pipe';
 import { SorterPipe } from './pipe/sorter.pipe';
 import { ReactiveFormsModule } from '@angular/forms';
 import { DataTableComponent } from './common/data-table/data-table/data-table.component';
+import { TableColSumPipe } from './pipe/table-col-sum.pipe';
 
 
 @NgModule({
@@ -43,7 +44,8 @@ import { DataTableComponent } from './common/data-table/data-table/data-table.co
     EditBillComponent,
     FilterPipe,
     SorterPipe,
-    DataTableComponent
+    DataTableComponent,
+    TableColSumPipe
   ],
   imports: [
     BrowserModule,
